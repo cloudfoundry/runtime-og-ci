d12fc73fc43ace862f14009b6a194361f0f66d4f
Michael Fraenkel <fraenkel@us.ibm.com>
2016-07-06 15:18:49 -0400

    New app-autoscaler unit CI

--- autoscaler/scripts/autoscaler-unit-tests.sh
+++ autoscaler/scripts/autoscaler-unit-tests.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 set -x
 
+service postgresql start
+
 cd app-autoscaler
 
-mvn test -Denv=unittest
+mvn package
+java -cp 'db/target/lib/*'  liquibase.integration.commandline.Main --username=postgres --changeLogFile=api/db/api.db.changelog.yml --url jdbc:postgresql://127.0.0.1/autoscaler --driver=org.postgresql.Driver update
+java -cp 'db/target/lib/*'  liquibase.integration.commandline.Main --username=postgres --changeLogFile=servicebroker/db/servicebroker.db.changelog.yml --url jdbc:postgresql://127.0.0.1/autoscaler --driver=org.postgresql.Driver update
+
+npm set progress=false
+
+pushd api
+npm install
+npm test
+popd
+
+pushd servicebroker
+npm install
+npm test
+popd
+
+export GOPATH=$PWD
+export PATH=$GOPATH/bin:$PATH
+go install github.com/onsi/ginkgo/ginkgo
+
+pushd src/metrics-collector
+ginkgo -r -race
+popd
