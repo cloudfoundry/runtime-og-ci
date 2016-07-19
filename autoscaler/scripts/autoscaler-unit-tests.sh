01e0ca36475ea081eb993e6fdc7f7d92b959bc82
Michael Fraenkel <fraenkel@us.ibm.com>
2016-07-19 14:51:39 -0400

    Quote the args

--- autoscaler/scripts/autoscaler-unit-tests.sh
+++ autoscaler/scripts/autoscaler-unit-tests.sh
@@ -8,7 +8,7 @@ psql postgres://postgres@127.0.0.1:5432 -c 'CREATE DATABASE autoscaler'
 
 cd app-autoscaler
 
-export POSTGRES_OPTS=--username=postgres --url=jdbc:postgresql://127.0.0.1/autoscaler --driver=org.postgresql.Driver
+POSTGRES_OPTS='--username=postgres --url=jdbc:postgresql://127.0.0.1/autoscaler --driver=org.postgresql.Driver'
 
 mvn package
 java -cp 'db/target/lib/*' liquibase.integration.commandline.Main $POSTGRES_OPTS --changeLogFile=api/db/api.db.changelog.yml update
