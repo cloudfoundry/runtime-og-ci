2a1a1b36611c3d84eb800a68d105599a292e1244
Michael Fraenkel <fraenkel@us.ibm.com>
2016-10-24 14:15:58 -0400

    Add integration tests

---
+++ autoscaler/scripts/integration-tests.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -x
+set -e
+
+service postgresql start
+psql postgres://postgres@127.0.0.1:5432 -c 'DROP DATABASE IF EXISTS autoscaler'
+psql postgres://postgres@127.0.0.1:5432 -c 'CREATE DATABASE autoscaler'
+
+cd app-autoscaler
+
+POSTGRES_OPTS='--username=postgres --url=jdbc:postgresql://127.0.0.1/autoscaler --driver=org.postgresql.Driver'
+
+mvn package
+java -cp 'db/target/lib/*' liquibase.integration.commandline.Main $POSTGRES_OPTS --changeLogFile=api/db/api.db.changelog.yml update
+java -cp 'db/target/lib/*' liquibase.integration.commandline.Main $POSTGRES_OPTS --changeLogFile=servicebroker/db/servicebroker.db.changelog.json update
+java -cp 'db/target/lib/*' liquibase.integration.commandline.Main $POSTGRES_OPTS --changeLogFile=scheduler/db/scheduler.changelog-master.yaml update
+java -cp 'db/target/lib/*' liquibase.integration.commandline.Main $POSTGRES_OPTS --changeLogFile=scheduler/db/quartz.changelog-master.yaml update
+java -cp 'db/target/lib/*' liquibase.integration.commandline.Main $POSTGRES_OPTS --changeLogFile=src/autoscaler/metricscollector/db/metricscollector.db.changelog.yml update
+java -cp 'db/target/lib/*' liquibase.integration.commandline.Main $POSTGRES_OPTS --changeLogFile=src/autoscaler/eventgenerator/db/dataaggregator.db.changelog.yml update
+java -cp 'db/target/lib/*' liquibase.integration.commandline.Main $POSTGRES_OPTS --changeLogFile=src/autoscaler/scalingengine/db/scalingengine.db.changelog.yml update
+
+npm set progress=false
+
+pushd api
+npm install
+popd
+
+pushd scheduler
+mvn package -DskipTests
+popd
+
+pushd test/integration/api
+npm install
+npm run integration
+popd
+
